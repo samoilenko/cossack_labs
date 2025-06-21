015bd7b147767f78afc35d905913ccfd6f949dd1
Serhii Samoilenko <s.samoilenko@gmail.com>
2025-06-21 15:40:15 +0300

    add dummy sensor

---
+++ sensor/infrastructure/dummy_sensor.go
@@ -0,0 +1,13 @@
+// Package infrastructure provides concrete implementation of sensor domain entities.
+package infrastructure
+
+import "math/rand/v2"
+
+// DummySensor is a sensor implementation that generates random integer values
+type DummySensor struct {
+}
+
+// GetValue return a random integer value between 0 and 99
+func (d DummySensor) GetValue() int {
+	return rand.IntN(100)
+}
