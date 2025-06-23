ef6e68b0076b8712c96fbca88723b50df23cdb9b
Serhii Samoilenko <s.samoilenko@gmail.com>
2025-06-23 10:14:50 +0300

    add additional address validations

--- sensor/domain/address.go
+++ sensor/domain/address.go
@@ -1,6 +1,9 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+	"net/url"
+)
 
 // Address of the telemetry sink.
 type Address string
@@ -11,5 +14,19 @@ func NewAddress(address string) (Address, error) {
 	if len(address) == 0 {
 		return "", errors.New("address cannot be empty")
 	}
+
+	parsedURL, err := url.Parse(address)
+	if err != nil {
+		return "", errors.New("address must be a valid URL")
+	}
+
+	if parsedURL.Scheme == "" {
+		return "", errors.New("address must include http:// or https:// scheme")
+	}
+
+	if parsedURL.Host == "" {
+		return "", errors.New("address must include a valid host")
+	}
+
 	return Address(address), nil
 }
